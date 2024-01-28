3b7dbaa20ce5fc1328fbfa397aa99abdfe6a3386
s21sd <srivastavasunny359@gmail.com>
2024-01-28 22:25:10 +0530

    Done

--- Notesapp/vite.config.ts
+++ Notesapp/vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    chunkSizeWarningLimit: 1500, 
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          
+          if (id.includes('some-library')) {
+            return 'some-library';
+          }
+        },
+      },
+    },
+  },
+});
